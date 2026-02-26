aae9d0a6f9f2567221ffe6b21ff3b203b114f44e
Reannu <reannub@gmail.com>
2026-02-26 21:05:21 +0800

    Add Search Button Prototype

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLineEdit, QPushButton
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("MVR Inventory")
-
         self.resize(800, 600)
+
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        layout = QVBoxLayout(central_widget)
+
+        # Input Field
+        self.input_field = QLineEdit()
+        self.input_field.setPlaceholderText("Enter SKU")
+
+        # Input Button
+        self.submit_button = QPushButton("Search")
+
+        #Add to MainLayout
+        layout.addWidget(self.input_field)
+        layout.addWidget(self.submit_button)
+
+
 
 
 if __name__ == "__main__":
