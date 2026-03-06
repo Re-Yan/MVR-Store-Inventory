283fdd6eca4f68f9875da7400035487ebe028f27
Reannu <reannub@gmail.com>
2026-03-06 18:23:03 +0800

    add .pycache folders to gitignore

--- ui/main.py
+++ ui/main.py
@@ -1,44 +1,6 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QLabel 
-
-class InputWidget(QWidget):
-    def __init__(self, text, buttonText):
-        super().__init__()
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-        self.resize(600, 400)
-
-        self.input_field = QLineEdit()
-        self.input_field.setPlaceholderText(text)
-
-        self.input_button = QPushButton(buttonText)
-        layout.addWidget(self.input_field)
-        layout.addWidget(self.input_button)
-
-class LogSection(QWidget):
-    def __init__(self, lineText, buttonText, label):
-        super().__init__()
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-
-        label = QLabel(label)
-        log_section = InputWidget(lineText, buttonText)
-
-        layout.addWidget(label)
-        layout.addWidget(log_section)
-    
-class SearchSection(QWidget):
-    def __init__(self, lineText, buttonText, label):
-        super().__init__()
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-
-        label = QLabel(label)
-        search_section = InputWidget(lineText, buttonText)
-
-        layout.addWidget(label)
-        layout.addWidget(search_section)
-
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget,  QHBoxLayout
+from widgets import  LogSection, SearchSection
 
 class MainWindow(QMainWindow):
     def __init__(self):
