f91d2b856e99525f3d62cc0e42f40475e5f8b4f2
Reannu <reannub@gmail.com>
2026-03-31 14:24:34 +0800

    Separate Log Section With Its Own Page

--- ui/main.py
+++ ui/main.py
@@ -16,26 +16,30 @@ class MainWindow(QMainWindow):
 
         central_widget = QWidget()
         Main_layout = QHBoxLayout(central_widget)
-        log_Section = LogSection("Enter SKU", "Submit", "Log Section")
-        search_Section = SearchSection("Enter Part Number", "Search", "Search Section")        
-        Main_layout.addWidget(log_Section)
+        search_Section = SearchSection("Enter Part Number", "Search", "Search Section") 
         Main_layout.addWidget(search_Section)
         
         navigation_sideBar = QWidget()
         nav_layout = QVBoxLayout()
         navigation_sideBar.setLayout(nav_layout)
-        button1 = QPushButton("Home")
+
+        button1 = QPushButton("Search")
         button1.clicked.connect(lambda: self.stack.setCurrentIndex(0))
-        button2 = QPushButton("Transactions")
+        button2 = QPushButton("Log")
         button2.clicked.connect(lambda: self.stack.setCurrentIndex(1))
+        button3 = QPushButton("Transactions")
+        button3.clicked.connect(lambda: self.stack.setCurrentIndex(2))
+
         nav_layout.addWidget(button1)
         nav_layout.addWidget(button2)
+        nav_layout.addWidget(button3)
 
         transaction_section = transaction_page("Transaction Search")
-        
+        log_section = LogSection("Enter SKU", "Submit", "Log Section")
 
         self.stack = QStackedWidget()
         self.stack.addWidget(central_widget)
+        self.stack.addWidget(log_section)
         self.stack.addWidget(transaction_section)
 
         container_widget = QWidget()
