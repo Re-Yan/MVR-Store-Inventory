b2578b867c235ead3d1ab389e95ee0357d74dddf
Reannu <reannub@gmail.com>
2026-04-30 11:48:51 +0800

    Add restock_section to QStackWidget

--- ui/main.py
+++ ui/main.py
@@ -5,7 +5,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout,  QHBoxLayout, QStackedWidget, QPushButton 
-from widgets import  LogSection, SearchSection, transaction_page
+from widgets import  LogSection, SearchSection, transaction_page, restock_page
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -19,6 +19,7 @@ class MainWindow(QMainWindow):
         
         transaction_section = transaction_page("Transaction Search")
         log_section = LogSection("Log Section")
+        restock_section = restock_page("Restock Section")
         
         # Create a callback that fills the SKU field when a suggestion is selected
         def on_suggestion_selected(sku):
@@ -37,15 +38,19 @@ class MainWindow(QMainWindow):
         button2.clicked.connect(lambda: self.stack.setCurrentIndex(1))
         button3 = QPushButton("Transactions")
         button3.clicked.connect(lambda: self.stack.setCurrentIndex(2))
-
+        button4 = QPushButton("Restock")
+        button4.clicked.connect(lambda: self.stack.setCurrentIndex(3))
+    
         nav_layout.addWidget(button1)
         nav_layout.addWidget(button2)
         nav_layout.addWidget(button3)
+        nav_layout.addWidget(button4)
 
         self.stack = QStackedWidget()
         self.stack.addWidget(central_widget)
         self.stack.addWidget(log_section)
         self.stack.addWidget(transaction_section)
+        self.stack.addWidget(restock_section)
 
         container_widget = QWidget()
         container_layout = QHBoxLayout()
