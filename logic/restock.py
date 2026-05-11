95e1c6bdeb3ac803a27983213cacbde12617f2b5
Reannu <reannub@gmail.com>
2026-05-11 14:01:01 +0800

    Remove date_carried_over parameter in add_request_item function
    - passed in None to date_carried_over since all requested items are
     always in PENDING status

--- logic/restock.py
+++ logic/restock.py
@@ -4,18 +4,33 @@ from datetime import datetime
 
 DB = os.path.join(os.path.dirname(__file__), "..", "mvr_inventory.db")
 
-def get_curr_date(self):
+def get_curr_date():
         date = datetime.now()
         curr_formatted_date = date.strftime("%Y-%m-%d")
+        return curr_formatted_date
 
-def create_batch(self):
+def get_part_id_by_sku(sku):
+    with sqlite3.connect(DB) as conn:
+        cursor = conn.cursor()
+
+        query = """
+            SELECT id 
+            FROM parts
+            WHERE sku = ?
+        """
+
+        cursor.execute(query, (sku,))
+        part_id = cursor.fetchone()
+        return part_id[0] if part_id else None
+
+def create_batch():
     with sqlite3.connect(DB) as conn:
         cursor = conn.cursor()
         curr_date = get_curr_date()
         insert_batch = """
             INSERT INTO restock_batches (created_on, status, ordered_on, completed_on)
             VALUES (?, ?, ?, ?)
-            """
+        """
         
         # Create a Fresh New Request Batch
         cursor.execute(insert_batch, (
@@ -29,11 +44,10 @@ def create_batch(self):
         print(f"New Request Batch Created: Batch #{batch_id}")
         return batch_id
 
-def get_current_batch(self):
+def get_current_batch():
     # fetches the most recent open OPEN batch. If none exists, create one
     with sqlite3.connect(DB) as conn:
         cursor = conn.cursor()
-
 
         search_batch = """
             SELECT id
@@ -41,19 +55,19 @@ def get_current_batch(self):
             WHERE status = 'OPEN'
             ORDER BY created_on DESC, id DESC
             LIMIT 1
-            """
+        """
 
         try:
             cursor.execute(search_batch)
             result = cursor.fetchone()
 
             if not result:
-                return self.create_batch()
+                return create_batch()
 
         except sqlite3.Error as e:
             raise RuntimeError("cannot get batch data: {e}") from e
 
-def add_request_item(batch_id, part_id, quantity, status, date_carried_over, notes):
+def add_request_item(batch_id, part_id, quantity, status, notes):
     with sqlite3.connect(DB) as conn:
         cursor = conn.cursor()
 
@@ -61,7 +75,7 @@ def add_request_item(batch_id, part_id, quantity, status, date_carried_over, not
 
         insert_request_item = """
             INSERT INTO request_items (batch_id, part_id, quantity_requested, urgency_score, status, created_on, date_carried_over, notes)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
         """
 
         cursor.execute(insert_request_item, (
@@ -71,6 +85,6 @@ def add_request_item(batch_id, part_id, quantity, status, date_carried_over, not
             "1",
             status,
             curr_date,
-            date_carried_over,
+            None,
             notes
         ))
